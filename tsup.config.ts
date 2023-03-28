fb1d8a1d6f855c99b70706442473bd2e2aeed566
Michael Kreuzmayr <michael.kreuzmayr@gmail.com>
2023-03-28 20:35:45 +0200

    Fix vsix build, externalize vscode, replace deprecated packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/extension.ts", "src/web/webView.ts"],
   clean: true,
-  format: ["cjs", "esm"],
+  format: ["cjs"],
   sourcemap: true,
   minify: false,
+  bundle: true,
   outDir: "out",
+  platform: "node",
+  external: ["vscode"],
 });
